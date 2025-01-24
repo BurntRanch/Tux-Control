1134c12cad59d6630a040ace4e5219f3543b02a8
BurntRanch <69512353+BurntRanch@users.noreply.github.com>
2025-01-24 18:12:00 +0300

    Initial

---
+++ app/src/main/java/io/github/burntranch/tuxcontrol/MainActivity.kt
@@ -0,0 +1,35 @@
+package io.github.burntranch.tuxcontrol
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.burntranch.tuxcontrol.StartScreen
+import io.github.burntranch.tuxcontrol.ui.theme.TuxControlTheme
+
+class MainActivity : ComponentActivity() {
+    private fun onStartButtonClick() {
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TuxControlTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    StartScreen(
+                        modifier = Modifier.padding(innerPadding),
+                        onStartButtonClick = { onStartButtonClick(); }
+                    )
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/io/github/burntranch/tuxcontrol/StartScreen.kt
@@ -0,0 +1,35 @@
+package io.github.burntranch.tuxcontrol
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.burntranch.tuxcontrol.ui.theme.TuxControlTheme
+
+@Composable
+fun StartScreen(modifier: Modifier = Modifier, onStartButtonClick: () -> Unit = {}) {
+    Column (modifier = modifier.fillMaxWidth()) {
+        Text(text = "Tux Control", modifier = modifier.align(Alignment.CenterHorizontally))
+
+        Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Control your Linux PC remotely!", modifier = modifier)
+            Button(onClick = { onStartButtonClick() }, modifier = modifier.align(Alignment.CenterHorizontally)) {
+                Text("Get Started")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartScreenPreview() {
+    TuxControlTheme {
+        StartScreen()
+    }
+}
